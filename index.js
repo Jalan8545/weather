05e55ed25673b6358991d68f4887338b150a44eb
Jalandhar <Jalandhar143184@gmail.com>
2022-01-19 22:40:00 +0530

    first

---
+++ index.js
@@ -0,0 +1,38 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+const homeFile= fs.readFileSync("home.html","utf-8");
+const replaceVal=(tempVal,orgVal)=>{
+    let temperature=tempVal.replace("{%tempval%}",orgVal.main.temp);
+    temperature=temperature.replace("{%tempmin%}",orgVal.main.temp_min);
+    temperature=temperature.replace("{%tempmax%}",orgVal.main.temp_max);
+    temperature=temperature.replace("{%location%}",orgVal.name);
+    temperature=temperature.replace("{%country%}",orgVal.sys.country);
+    temperature=temperature.replace("{%tempstatus%}",orgVal.weather.main);
+    return temperature;
+
+}
+
+const server=http.createServer((req,res)=>{
+     if(req.url=="/"){
+       requests(
+           "http://api.openweathermap.org/data/2.5/weather?q=kanpur&units=metric&appid=166efe5fbbc66c1059854d689e96ce27",
+        
+        )
+        .on("data",(chunk)=> {
+            const objdata=JSON.parse(chunk);
+            const arrData=[objdata];
+            const realTimeData=arrData.map((val)=>
+            replaceVal(homeFile,val)).join("");
+            res.write(realTimeData);
+            console.log(arrData);
+            
+            })
+        .on("end",(err)=>{
+            if(err) return console.log("connection closed due to error",err); 
+            res.end();
+        });
+     }
+});
+server.listen(8000,"127.0.0.1");
